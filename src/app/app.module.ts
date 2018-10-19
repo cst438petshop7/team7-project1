fbb48d0dcf486c302f2e5cf76c2d37d2e66cde07
jayarellano6 <jaarellano@csumb.edu>
2018-10-18 19:34:12 -0700

    added empty components for each of the pages needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SigninPageComponent } from './signin-page/signin-page.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { ThankyouPageComponent } from './thankyou-page/thankyou-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsPageComponent,
+    HomePageComponent,
+    SigninPageComponent,
+    CheckoutPageComponent,
+    ProductsPageComponent,
+    ThankyouPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/thankyou-page/thankyou-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thankyou-page',
+  templateUrl: './thankyou-page.component.html',
+  styleUrls: ['./thankyou-page.component.scss']
+})
+export class ThankyouPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
