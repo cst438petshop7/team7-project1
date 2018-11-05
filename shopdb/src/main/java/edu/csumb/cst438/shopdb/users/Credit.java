0bd21dae0674ce8275ed0c56ddbe33ed183606d7
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-11-04 20:01:02 -0800

    Added spring-boot MongoDB

---
+++ shopdb/src/main/java/edu/csumb/cst438/shopdb/users/Credit.java
@@ -0,0 +1,14 @@
+package edu.csumb.cst438.shopdb.users;
+
+public class Credit {
+    private int credit;
+
+    public Credit (int credit) {
+        this.credit = credit;
+    }
+
+    public int getCredit() {
+        return this.credit;
+    }
+
+}
---
+++ shopdb/src/main/java/edu/csumb/cst438/shopdb/users/Username.java
@@ -0,0 +1,14 @@
+package edu.csumb.cst438.shopdb.users;
+
+public class Username {
+    private String username;
+
+    public Username (String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+}
