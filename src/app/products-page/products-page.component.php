f53beac72b6cb8a07b4a1b22e4659c30fabf8f70
jayarellano6 <jaarellano@csumb.edu>
2018-11-06 11:54:26 -0800

    some changes

--- src/app/products-page/products-page.component.php
+++ src/app/products-page/products-page.component.php
@@ -2,6 +2,8 @@
   <h2>Products</h2>
   <hr>
   <ul id="products" style="padding-top: 0px;">
+    <?php
+      for ($x = 0; $x <= 10; $x++) { ?>
     <li>
         <div id="product-left" style="float: left;">
           <img style="float: left;" src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350" width="100" />
@@ -14,7 +16,9 @@
           <br style="clear: both;"/>
         </div><br style="clear: both;"/>
     </li>
-    <li>
+
+    <?php } ?>
+    <!-- <li>
       <div id="product-left" style="float: left;">
         <img style="float: left;" src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350" width="100" />
         <p id="item-name"><a href="/product-details">Item Name</a></p><br style="clear: both;"/>
@@ -22,7 +26,7 @@
 
       <div id="product-right" style="float: right;">
         <div id="button-left" style="float: left;"><p style="margin: 0;">Price: $0.00<br/>items in stock: 0</p></div>
-        <div id="button-right" style="float: right;"><button class="button">add to cart</button></div>
+        <div id="button-right" style="sfloat: right;"><button class="button">add to cart</button></div>
         <br style="clear: both;"/>
       </div><br style="clear: both;"/>
   </li>
@@ -97,7 +101,7 @@
     <div id="button-right" style="float: right;"><button class="button">add to cart</button></div>
     <br style="clear: both;"/>
   </div><br style="clear: both;"/>
-</li>
+</li> -->
   </ul>
 </div>
 
