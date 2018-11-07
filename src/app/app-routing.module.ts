673b0b00c05ac3034c91ae5b6a1b79dbcbe6fdde
jayarellano6 <jaarellano@csumb.edu>
2018-11-06 18:05:31 -0800

    checkpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
+import { ThankyouPageComponent } from './thankyou-page/thankyou-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { SigninPageComponent } from './signin-page/signin-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: SigninPageComponent
+  },
+  {
+    path: 'products',
+    component: ProductsPageComponent
+  },
+  {
+    path: 'product-details',
+    component: DetailsPageComponent
+  },
+  {
+    path: 'checkout_cart',
+    component: CheckoutPageComponent
+  },
+  {
+    path: 'thankyou',
+    component: ThankyouPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
