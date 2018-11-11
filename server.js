78d8caac40980cf763280b65131bf07e56b6ab34
jayarellano6 <jaarellano@csumb.edu>
2018-11-10 19:28:05 -0800

    okok

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 
-const express = require('express');
+const express = require("express");
 const path = require('path');
 
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + "/dist/cst438-proj1"));
 
-app.get('/*', function(req,res) {
+app.get("/*", function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+"/dist/cst438-proj1/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
