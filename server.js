decc29dcf76c7be43c8bb8667fd1e848311df7bd
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-11-19 22:06:03 -0800

    More Heroku Fixes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/cst438-proj1"));
+app.use(express.static(__dirname + "/dist/cst438Proj1"));
 
 app.get("/*", function(req,res) {
 
-res.sendFile(path.join(__dirname+"/dist/cst438-proj1/index.html"));
+res.sendFile(path.join(__dirname+"/dist/cst438Proj1/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
