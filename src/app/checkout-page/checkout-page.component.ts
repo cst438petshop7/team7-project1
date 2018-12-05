47b062498728e87a03b3c2b8ae4624626ad9e134
jayarellano6 <jaarellano@csumb.edu>
2018-12-05 00:04:45 -0800

    ...

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -11,11 +11,18 @@ export class CheckoutPageComponent implements OnInit {
 
   constructor(private data: DataService) { }
   cartItems: Array<CartItem> = [];
+  total: number;
   //
   ngOnInit() {
     this.cartItems = JSON.parse(this.data.cart.getItem('cart'));
   }
   product(n1, n2) {
     return n1 * n2;
+  }
+  totalPrice() {
+    this.cartItems.forEach(element => {
+      this.total += this.product(element.amount, element.price);
+    });
+    return this.total;
   }
 }
