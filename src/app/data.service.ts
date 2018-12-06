4701ace6ef8391ec6158bad128ff7c4dd8b44bc5
jayarellano6 <jaarellano@csumb.edu>
2018-12-05 23:31:34 -0800

    ...

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -68,7 +68,7 @@ export class DataService {
 
     console.log(JSON.stringify(this.finalCart));
     return this.http.put('https://finalize-order-service.herokuapp.com/finalize/' +
-    this.userIn.getItem('key') + '/' + this.finalCart, JSON.stringify({}), {headers: putHeader});
+    this.userIn.getItem('key') + this.finalCart, JSON.stringify({}), {headers: putHeader});
   }
   getUserSess() {
     return this.userIn.getItem('key');
