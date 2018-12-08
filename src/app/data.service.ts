a680283878f422ea58b9b49da9da62203fd745eb
jayarellano6 <jaarellano@csumb.edu>
2018-12-07 19:40:43 -0800

    sign in

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -69,6 +69,9 @@ export class DataService {
         } else {
           alert('invalid username or password');
         }
+      },
+      error => {
+        alert('invalid username or password');
       }
     );
   }
